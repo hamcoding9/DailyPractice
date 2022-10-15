89b1361a8596bea566decac72955ba802f9fb52e
Dora <dientes8520@gmail.com>
2022-10-15 20:50:40 +0900

    [코드 수정]안전영역

--- Baekjoon/2468-안전영역.py
+++ Baekjoon/2468-안전영역.py
@@ -8,11 +8,10 @@ for i in range(n):
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def dfs(graph, x, y):
+def dfs(x, y, visit):
     stack = list()
     stack.append((x, y))
-    graph[x][y] = 0 # 그래프에 방문 처리
-    count = 1
+    visit[x][y] = 1 # 그래프에 방문 처리
     while stack:
         cx, cy = stack.pop()
         for i in range(4):
@@ -20,21 +19,20 @@ def dfs(graph, x, y):
             ny = cy + dy[i]
             if nx >= n or nx < 0 or ny >= n or ny < 0:
                 continue
-            if graph[nx][ny] > sink:
+            if graph[nx][ny] > sink and visit[nx][ny] == 0:
                 stack.append((nx, ny))
-                graph[nx][ny] = 0
-                count += 1
-    return count
+                visit[nx][ny] = 1
                 
 safe_area = []
-for i in range(0, max(map(max, graph))+1): # 모든 sink 경우를 고려(아무 지역도 물에 잠기지 않는 경우를 고려해서 0도 포함)
+for i in range(max(map(max, graph))+1): # 모든 sink 경우를 고려(아무 지역도 물에 잠기지 않는 경우를 고려해서 0도 포함)
+    count = 0
     sink = i
-    cnt = [] # 안전한 영역을 담음
-    g = copy.deepcopy(graph) # graph를 복사함
+    visit = [[0]*n for i in range(n)]
     for j in range(n):
         for k in range(n):
-            if g[j][k] > sink:
-                cnt.append(dfs(g, j, k))
-    safe_area.append(len(cnt))
-    
+            if graph[j][k] > sink and visit[j][k]==0:
+                dfs(j, k, visit)
+                count += 1
+    safe_area.append(count)
+
 print(max(safe_area))
